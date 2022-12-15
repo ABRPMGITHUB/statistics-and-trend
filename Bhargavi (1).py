2e338d8acbdbf3ce040cdad51af9a660eb60bc94
ABRPMGITHUB <118008218+ABRPMGITHUB@users.noreply.github.com>
2022-12-15 05:41:58 +0530

    Update Bhargavi (1).py

--- Bhargavi (1).py
+++ Bhargavi (1).py
@@ -20,3 +20,30 @@ from matplotlib import pyplot as mat_plot
 
 # #Make Function 
 
+# In[2]:
+
+
+def read_df(Data_filename):
+    data_ag1=pd.read_csv(Data_filename,skiprows=4)
+    #read datset & skip 4 rows 
+    data_ag2=data_ag1.drop(['Indicator Code', 'Unnamed: 66', 'Country Code', 'Indicator Name'],axis=1) # unused columns drop
+    data_ag3=data_ag2.set_index("Country Name")
+    # transpose dataset 
+    data_ag4=data_ag3.T
+    data_ag4.reset_index(inplace=True)
+    data_ag4.rename(columns = {'index':'Year'}, inplace = True) 
+    return data_ag1,data_ag4
+
+
+# In[3]:
+
+
+Data_filename="/content/API_1_DS2_en_csv_v2_4550063.csv" # dataset path 
+df_ag1,df_ag2=read_df(Data_filename)
+df_ag1.head(8) # visualizaing dataset 
+
+
+# In[4]:
+
+
+df_ag2.head(8)#visualizaing dataset 
