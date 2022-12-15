d2b56589afa34d1782ef58188f1773426c9718de
ABRPMGITHUB <118008218+ABRPMGITHUB@users.noreply.github.com>
2022-12-15 05:40:28 +0530

    Update Bhargavi (1).py

--- Bhargavi (1).py
+++ Bhargavi (1).py
@@ -1,1 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 15 05:39:26 2022
+
+@author: bhargavi
+"""
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# #Importing library
+
+# In[1]:
+
+
+import pandas as pd 
+import numpy as np 
+from matplotlib import pyplot as mat_plot 
+
+
+# #Make Function 
 
